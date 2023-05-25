e65fbf666440a30e6dfb65e28f2d45286a27f9c0
Evertz Hamilton <hamiltonevertz99@gmail.com>
2023-05-25 10:38:24 -0400

    feat: 'DB Connection Completed'

--- src/config/DBConnection.js
+++ src/config/DBConnection.js
@@ -1,1 +1,16 @@
 const {default: mongoose} = require('mongoose');
+
+const DBConnect = () => {
+  try {
+    const conn = mongoose.connect(process.env.DATABASE_URL);
+  
+    console.log('Database Connected Successfully');
+
+  } catch (error) {
+    
+    console.log('Database error',error.message);
+
+  }
+};
+
+module.exports = DBConnect;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const DBConnect = require('./config/DBConnection');
 const app = express();
 const dotenv = require('dotenv').config();
+
+//Conection With DataBase
+DBConnect()
 
 const port = process.env.PORT || 9000;
 
