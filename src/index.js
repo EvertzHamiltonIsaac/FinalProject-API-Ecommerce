7bb0172b03c840f8c28010dabfe1993ee5bc5ce2
Evertz Hamilton <hamiltonevertz99@gmail.com>
2023-05-25 09:59:34 -0400

    feat: 'dev branch created'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
 
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  
+  console.log(`Server is running on port ${port}`);
+})
