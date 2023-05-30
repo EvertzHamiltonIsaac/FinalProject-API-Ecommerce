8a3226cce178512a19e25329a6369e2463a23316
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-05-29 20:37:30 -0400

    feat: 'Auth Completed'

--- src/MVC/controllers/user.controller.js
+++ src/MVC/controllers/user.controller.js
@@ -1,8 +1,9 @@
+const { TokenGenerator } = require("../../config/JwtToken/jwtToken");
 const User = require("../models/user.model");
-
 const asyncHandler = require("express-async-handler");
 
-const createUser = asyncHandler(async (req, res) => {
+// TODO: Controllers For Auth.
+const registerUser = asyncHandler(async (req, res) => {
   const body = req.body;
   const findUser = await User.findOne({ email: body.email });
 
@@ -10,7 +11,7 @@ const createUser = asyncHandler(async (req, res) => {
     const userData = { ...body };
     try {
       const newUser = await User.create(userData);
-      res.status(200).send({
+      res.status(201).send({
         message: "User created",
         status: "200",
         data: [{...userData, password: ''}],
@@ -25,5 +26,26 @@ const createUser = asyncHandler(async (req, res) => {
     throw new Error("User Already Exists");
   }
 });
+const loginUser = asyncHandler(async (req, res) => {
+  const body = req.body;
 
-module.exports = { createUser };
+  const findUser = await User.findOne({ email: body.email });
+  if(findUser && await findUser.isPasswordMatched(body.password)){
+    res.status(202).send({
+      message: `Welcome to Ginger ${findUser.firstName}`,
+      data: {...findUser?._doc, password: '' }, 
+      sesstionToken: TokenGenerator(findUser?._doc._id)
+    })
+  } else {    
+    res.status(401).send({
+      fields: {
+        email: "Email@gmail.com",
+        password: "password"
+      }
+    })
+  }
+})
+
+
+
+module.exports = { registerUser, loginUser };
