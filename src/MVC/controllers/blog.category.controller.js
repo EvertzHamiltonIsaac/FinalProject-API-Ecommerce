30b7f321f5c52d77581b3c067d7f922f3eafbc2f
saulmarte <saulkjh1227@gmail.com>
2023-06-08 17:37:44 -0400

    Created blogCategory and especified prodCategory

--- src/MVC/controllers/blog.category.controller.js
+++ src/MVC/controllers/blog.category.controller.js
@@ -1,10 +1,10 @@
-const Category = require("../models/category.model");
+const blogCategory = require("../models/blog.category.model");
 const asyncHandler = require("express-async-handler");
 const validateMongoId = require("../../utils/validateMongoId");
 
 const createCategory = asyncHandler(async (req, res) => {
   try {
-    const newCategory = await Category.create(req.body);
+    const newCategory = await blogCategory.create(req.body);
     res.status(200).send(newCategory);
   } catch (error) {
     res.status(400).send(error);
@@ -15,7 +15,7 @@ const updateCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoId(id);
   try {
-    const updatedCategory = await Category.findOneandUpdate(id, req.body, {
+    const updatedCategory = await blogCategory.findOneandUpdate(id, req.body, {
       new: true,
     });
     res.status(200).send(updatedCategory);
@@ -28,7 +28,7 @@ const deleteCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoId(id);
   try {
-    const deletedCategory = await Category.findOneAndDelete(id);
+    const deletedCategory = await blogCategory.findOneAndDelete(id);
     res.status(200).send(deletedCategory);
   } catch (error) {
     res.status(400).send(error);
@@ -39,7 +39,7 @@ const getCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoId(id);
   try {
-    const getaCategory = await Category.findById(id);
+    const getaCategory = await blogCategory.findById(id);
     res.status(200).send(getaCategory);
   } catch (error) {
     res.status(400).send(error);
@@ -48,7 +48,7 @@ const getCategory = asyncHandler(async (req, res) => {
 
 const getAllCategories = asyncHandler(async (req, res) => {
   try {
-    const getallCategory = await Category.find();
+    const getallCategory = await blogCategory.find();
     res.status(200).send(getallCategory);
   } catch (error) {
     res.status(400).send(error);
