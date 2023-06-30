08dc32910123348bb5209fa972a857c0b2c2ab86
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-06-29 20:14:31 -0400

    feat: route of middleware of upload changed

--- src/middlewares/uploadImages.js
+++ src/middlewares/uploadImages.js
@@ -2,10 +2,10 @@ const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
 const fs = require("fs");
-//path.join(__dirname, "../public/images/")
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "/public/images/"));
+    cb(null, path.join(__dirname, "./src/public/images/"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -38,10 +38,10 @@ const productImgResize = async (req, res, next) => {
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
         .toFile(
-          `../public/images/products/${file.filename}`
+          path.join(__dirname, `../public/images/products/${file.filename}`)
         );
       fs.unlinkSync(
-        `../public/images/products/${file.filename}`
+        path.join(__dirname, `../public/images/products/${file.filename}`)
       );
     })
   );
