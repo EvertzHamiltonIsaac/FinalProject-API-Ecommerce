9843a76d9ea7ac682a3fd775bc8f9a4094577761
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-06-29 20:52:22 -0400

    feat: route of middleware of upload changed

--- src/middlewares/uploadImages.js
+++ src/middlewares/uploadImages.js
@@ -38,10 +38,10 @@ const productImgResize = async (req, res, next) => {
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
         .toFile(
-          path.join(__dirname, `../../public/images/products/${file.filename}`)
+          path.join(__dirname, `../../public/images/${file.filename}`)
         );
       fs.unlinkSync(
-        path.join(__dirname, `../../public/images/products/${file.filename}`)
+        path.join(__dirname, `../../public/images/${file.filename}`)
       );
     })
   );
