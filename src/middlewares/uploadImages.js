0742c64709ea7805aa5ab51396d3c18b527aa3ba
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-06-29 20:38:58 -0400

    feat: route of middleware of upload changed

--- src/middlewares/uploadImages.js
+++ src/middlewares/uploadImages.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 //path.join(__dirname, "./src/public/images/")
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../public/images/"));
+    cb(null, path.join(__dirname, "../../public/images/"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
