95cc973aa5ccb696a619cbb6592a11679d0fb1de
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-06-29 23:33:58 -0400

    feat: making change in uploadImages

--- src/middlewares/uploadImages.js
+++ src/middlewares/uploadImages.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log(path.join(__dirname, "../public/images"));
-    cb(null, path.join(__dirname, "../public/images"));
+    cb(null, path.join(__dirname, "../../public/images"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
