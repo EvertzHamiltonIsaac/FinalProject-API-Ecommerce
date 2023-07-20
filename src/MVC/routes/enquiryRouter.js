9fc61f32932396535154b410540faf263af19926
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-07-20 11:23:58 -0400

    feat: authmiddleware deleted of create enquiry

--- src/MVC/routes/enquiryRouter.js
+++ src/MVC/routes/enquiryRouter.js
@@ -11,7 +11,7 @@ const { authMiddleware, isAdmin } = require("../../middlewares/authMiddleware");
 
 router.get("/enquiry/", getAllEnquiry);
 router.get("/enquiry/:id", getEnquiryById);
-router.post("/enquiry/", authMiddleware, createEnquiry);
+router.post("/enquiry/", createEnquiry);
 router.put("/enquiry/:id", authMiddleware, isAdmin, updateEnquiry);
 router.delete("/enquiry/:id", authMiddleware, isAdmin, deleteEnquiry);
 
