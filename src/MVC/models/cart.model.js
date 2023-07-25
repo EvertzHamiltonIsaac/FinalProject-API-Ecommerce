fa19b7ca0d4b257da22c23eb69e1e3ade9620818
saulmarte <saulkjh1227@gmail.com>
2023-07-24 22:40:45 -0400

    Changed cart model and cart controller

--- src/MVC/models/cart.model.js
+++ src/MVC/models/cart.model.js
@@ -1,25 +1,31 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-const cartSchema = new mongoose.Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-      },
-      count: Number,
-      color: String,
-      price: Number
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-  ],
-  cartTotal: Number,
-  totalAfterDiscount: Number,
-  orderBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  }
-}, {timestamps: true});
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    price: {
+      type: mongoose.Schema.Types.Number,
+      ref: "Price",
+    },
+    quantity: {
+      type: mongoose.Schema.Types.Number,
+      ref: "Quantity",
+    },
+    color: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Color",
+    },
+  },
+  { timestamps: true }
+);
 
 //Export the model
 module.exports = mongoose.model("Cart", cartSchema);
