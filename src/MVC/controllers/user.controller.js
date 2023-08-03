696eda554f2d4169cc919540a0ed916f64f0fee9
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-08-03 11:32:02 -0400

    feat: Delete Cart in Create Order

--- src/MVC/controllers/user.controller.js
+++ src/MVC/controllers/user.controller.js
@@ -201,7 +201,7 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
       to: email,
       text: "Hey User",
       subject: "Forgot Password Link",
-      htm: <div>Hola</div>,
+      htm: '<div>Hola</div>',
     };
 
     sendEmail(data);
